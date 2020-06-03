f76bb9051365ec170f342117bad3199d5b717a78
Matthew Richards <matthew.richards@stfc.ac.uk>
2020-06-03 13:20:08 +0100

    Add activities which are opened when the associated buttons are clicked

--- FiveASideTeamPickerApp/MainActivity.cs
+++ FiveASideTeamPickerApp/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
 namespace FiveASideTeamPickerApp
 {
@@ -14,6 +15,37 @@ namespace FiveASideTeamPickerApp
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            // Getting buttons on the main activity
+            Button instructionsButton = FindViewById<Button>(Resource.Id.instructionsButton);
+            Button registerTeamButton = FindViewById<Button>(Resource.Id.registerTeamButton);
+            Button pickTeamsButton = FindViewById<Button>(Resource.Id.pickTeamsButton);
+            Button adminInterfaceButton = FindViewById<Button>(Resource.Id.adminInterfaceButton);
+
+            // Open instructions page when relevant button clicked
+            instructionsButton.Click += (sender, args) =>
+            {
+                StartActivity(typeof(InstructionsActivity));
+            };
+
+            // Open page to register a team when relevant button clicked
+            registerTeamButton.Click += (sender, args) =>
+            {
+                // Open register teams page
+                StartActivity(typeof(RegisterTeamActivity));
+            };
+
+            // Open page to select players for teams when relevant button clicked
+            pickTeamsButton.Click += (sender, args) =>
+            {
+                StartActivity(typeof(PickTeamsActivity));
+            };
+
+            // Open admin interface when relevant button clicked
+            adminInterfaceButton.Click += (sender, args) =>
+            {
+                StartActivity(typeof(AdminInterfaceActivity));
+            };
         }
     }
 }
