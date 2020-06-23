8d2e427897eb390786837100b72aaa4783f48887
Matthew Richards <matthew.richards@stfc.ac.uk>
2020-06-23 18:05:20 +0100

    Add option for a fantasy team to be deleted
    - This feature appears when a user attempts to register attempts to
    register a third team, but this feature means they must delete one of
    the fantasy teams before doing so

--- FiveASideTeamPickerApp/DeleteFantasyTeamActivity.cs
+++ FiveASideTeamPickerApp/DeleteFantasyTeamActivity.cs
@@ -9,17 +9,50 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using FantasyTeamsDBSharedCode;
+using FantasyTeamsDBSharedCode.SQLite_Implementation;
 
 namespace FiveASideTeamPickerApp
 {
     [Activity(Label = "DeleteFantasyTeamActivity")]
     public class DeleteFantasyTeamActivity : Activity
     {
+        SQLiteFantasyTeamRepository fantasyTeamRepository;
+
+        public DeleteFantasyTeamActivity()
+        {
+            fantasyTeamRepository = new SQLiteFantasyTeamRepository();
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.DeleteFantasyTeam);
 
-            // Create your application here
+            Button deleteFantasyTeam1Button = FindViewById<Button>(Resource.Id.deleteFantasyTeam1Button);
+            Button deleteFantasyTeam2Button = FindViewById<Button>(Resource.Id.deleteFantasyTeam2Button);
+
+            List<FantasyTeam> allFantasyTeams = fantasyTeamRepository.GetAllFantasyTeams();
+
+            deleteFantasyTeam1Button.Text = allFantasyTeams[0].FantasyTeamName;
+            deleteFantasyTeam2Button.Text = allFantasyTeams[1].FantasyTeamName;
+
+            deleteFantasyTeam1Button.Click += (sender, args) =>
+            {
+                DeleteSelectedFantasyTeam(allFantasyTeams[0]);
+                Finish();
+            };
+
+            deleteFantasyTeam2Button.Click += (sender, args) =>
+            {
+                DeleteSelectedFantasyTeam(allFantasyTeams[1]);
+                Finish();
+            };
+        }
+
+        void DeleteSelectedFantasyTeam(FantasyTeam fantasyTeam)
+        {
+            fantasyTeamRepository.RemoveFantasyTeam(fantasyTeam);
         }
     }
 }
